0b5eed52c5576aa77cc567a6658a230cf3dea745
SAFFR0NM0NEY <72283367+SAFFR0NM0NEY@users.noreply.github.com>
2025-05-20 15:18:11 +0200

    Student version test
    testing student version to confirm code

--- js/script.js
+++ js/script.js
@@ -19,10 +19,3 @@ fetch("data/products.xml")
     }
     document.getElementById("product-list").innerHTML = output;
   });
-
-function addToCart(name, price) {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  cart.push({ name, price });
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert(`${name} added to cart`);
-}
